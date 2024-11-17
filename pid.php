ba13bcaa6febbd0e6ba9e5b88e58a6cacd885b61
fkomarek <filip@fkomarek.eu>
2024-11-17 01:51:27 +0100

    ADD: přesměrování na počasí při kliknutí

--- pid.php
+++ pid.php
@@ -5,7 +5,7 @@
     <title>Smart panel: PID</title>
     <link rel="icon" type="image/x-icon" href="./favicon.ico">
     <meta charset='utf-8'>
-    <meta http-equiv="refresh" content="20;url=pid.php">
+    <meta http-equiv="refresh" content="20;url=pocasi.php">
 </head>
 
 <body>
@@ -90,6 +90,13 @@
         <pre id="testOutput"></pre>
 
         <script src="hodiny.js"></script>
+
+        <script>
+            // Funkce pro přesměrování na jinou stránku
+            document.addEventListener("click", function () {
+                window.location.href = "http://localhost/pocasi.php";  // Změň na URL, kam chceš přesměrovat
+            });
+        </script>
 
     </div>
 
