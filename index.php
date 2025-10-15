6d450a28e2db17c5f35afe6ed4b72b07da6b4361
filip2cz <filip@fkomarek.eu>
2025-10-15 13:31:33 +0000

    EDIT: index.php bez bootstrap

--- index.php
+++ index.php
@@ -11,15 +11,6 @@
 
     <link rel="stylesheet" type="text/css" href="./main.css">
 
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-        </script>
-
-    <div class="container">
-
         <h1>Device info</h1>
 
         <p>Local IP adress:
@@ -33,8 +24,6 @@
             ?>
 
         </p>
-
-    </div>
 
 </body>
 
