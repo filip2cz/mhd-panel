8494a3ac7bb7094ea5cacd1c12da2c9500c23030
filip2cz <filip@fkomarek.eu>
2026-01-11 20:31:14 +0100

    ADD: phpdocs

---
+++ docs/api/php/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\ziskejTeplotu\u0028\u0029",
+            "name": "ziskejTeplotu",
+            "summary": "Z\u00EDsk\u00E1\u0020aktu\u00E1ln\u00ED\u0020teplotu\u0020ze\u0020zadan\u00E9\u0020URL\u0020adresy.",
+            "url": "namespaces/default.html#function_ziskejTeplotu"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
