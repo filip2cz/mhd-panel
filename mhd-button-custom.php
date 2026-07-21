84ade7eb4ece386679c2b80efa2e9d6f0fc54384
filip2cz <filip@fkomarek.eu>
2026-07-21 22:37:42 +0200

    ADD: mhd-button-custom.php page

---
+++ mhd-button-custom.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='cs' data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MHD Tabule</title>
+</head>
+
+<body>
+
+    <link rel="stylesheet" type="text/css" href="./main.css">
+
+    <!-- Put whatever you want here, for example: -->
+
+    <h2><a href="https://data.pid.cz/departures/?ids=U1500Z1;U1500Z2&title=%C5%A0estajovice%2CZa+Stodolami" id="odkaz" class="vetsiText">Šestajovice,Za Stodolami</a></h2>
+    <h2><a href="https://data.pid.cz/departures/?ids=U1604Z1;U1604Z2&title=%C5%A0estajovice%2Cu+%C5%A1koly" id="odkaz" class="vetsiText">Šestajovice,u školy</a></h2>
+    <h2><a href="https://data.pid.cz/departures/?ids=U454Z3;U454Z3P;U454Z4;U454Z4P&title=N%C3%A1dra%C5%BE%C3%AD+Kl%C3%A1novice-sever" id="odkaz" class="vetsiText">Nádraží Klánovice-sever</a></h2>
+    <h2><a href="https://data.pid.cz/departures/?ids=U454Z301&title=Praha-Kl%C3%A1novice" id="odkaz" class="vetsiText">Praha-Klánovice</a></h2>
+    <h2><a href="https://data.pid.cz/departures/?ids=U454Z1;U454Z1P;U454Z2;U454Z2P;U454Z71;U454Z71P;U454Z72;U454Z72P&title=N%C3%A1dra%C5%BE%C3%AD+Kl%C3%A1novice" id="odkaz" class="vetsiText">Nádraží Klánovice</a></h2>
+
+</body>
+
+</html>
